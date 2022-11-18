f005dbd64fe1d2d191611aa0cd97687672a98c5c
798654 <a798654@gmail.com>
2022-11-19 07:19:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,7 +25,7 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 # rm -rf ./feeds/luci/applications/luci-app-qbittorrent
 rm -rf ./feeds/luci/applications/luci-app-serverchan
 # rm -rf ./package/diy-ziyong/adguardhome
-rm -rf ./feeds/packages/net/adguardhome
+# rm -rf ./feeds/packages/net/adguardhome
 rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/packages/net/mosdns
 rm -rf ./feeds/luci/luci-theme-argon
@@ -39,7 +39,7 @@ rm -rf ./package/openwrt-passwall/v2ray-geodata
 rm -rf ./package/ssr/v2ray-geodata
 ##
 rm -rf ./package/sirpdboy-package/smartdns
-# rm -rf ./package/sirpdboy-package/adguardhome
+rm -rf ./package/sirpdboy-package/adguardhome
 rm -rf ./package/sirpdboy-package/luci-app-dockerman
 rm -rf ./package/sirpdboy-package/luci-app-autotimeset
 # rm -rf ./feeds/packages/net/https-dns-proxy
