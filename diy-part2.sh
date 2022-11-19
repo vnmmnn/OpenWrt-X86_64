0ad3b116285712d545c52d2e9fa1164d1a0104b7
798654 <a798654@gmail.com>
2022-11-20 03:26:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,7 +37,7 @@ rm -rf ./feeds/packages/net/aria2
 # rm -rf ./package/diy-ziyong/adguardhome
 rm -rf ./feeds/luci/applications/luci-app-adbyby-plus
 rm -rf ./package/sirpdboy-package/luci-lib-ipkg
-rm -rf ./package/sirpdboy-package/luci-app-autotimeset
+# rm -rf ./package/sirpdboy-package/luci-app-autotimeset
 ##
 rm -rf ./package/openwrt-passwall/v2ray-geodata
 rm -rf ./package/ssr/v2ray-geodata
