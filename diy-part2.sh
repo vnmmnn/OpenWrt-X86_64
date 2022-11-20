36daa032d48318037c5fe776fead7c9b9073b245
798654 <a798654@gmail.com>
2022-11-20 14:17:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,8 +29,8 @@ rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/packages/net/mosdns
 rm -rf ./feeds/luci/luci-theme-argon
 ##
-rm -rf ./feeds/luci/applications/luci-app-aria2
-rm -rf ./feeds/packages/net/aria2
+# rm -rf ./feeds/luci/applications/luci-app-aria2
+# rm -rf ./feeds/packages/net/aria2
 # rm -rf ./package/diy-ziyong/alist-18.06/luci-app-alist
 # rm -rf ./package/diy-ziyong/alist-18.06/alist
 # rm -rf ./package/diy-ziyong/luci-app-adguardhome
