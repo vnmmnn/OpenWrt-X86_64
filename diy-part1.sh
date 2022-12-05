94b0d983eb2fe5f0bf4ac9a2e6685ec4dfb5522c
798654 <a798654@gmail.com>
2022-12-06 01:39:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 # sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git  package/lua-maxminddb
-# git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
+git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
 # git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 # git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
