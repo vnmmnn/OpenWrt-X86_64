4e52d4f8a7116d201b3818f4913313af615b33c2
798654 <a798654@gmail.com>
2022-12-11 16:01:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -34,11 +34,12 @@ svn co https://github.com/firker/diy-ziyong/trunk/luci-app-wrtbwmon-zhcn package
 svn co https://github.com/firker/diy-ziyong/trunk/luci-app-onliner package/diy-ziyong/luci-app-onliner
 svn co https://github.com/firker/diy-ziyong/trunk/wrtbwmon package/diy-ziyong/wrtbwmon
 svn co https://github.com/firker/diy-ziyong/trunk/theme package/diy-ziyong/theme
+svn co https://github.com/QiuSimons/openwrt-mos/trunk/v2ray-geodata package/openwrt-mos/v2ray-geodata
 ###
 # git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-bypass
 # sed -i 's/luci-lib-ipkg/luci-base/g' package/openwrt-bypass/luci-app-bypass/Makefile
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
+# git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
 git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
 git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
