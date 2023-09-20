23b41aaa0619e893e50c0e94dfd7d2409d8a1380
798654 <a798654@gmail.com>
2023-09-20 23:46:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,7 +21,7 @@ sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefi
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-git clone https://github.com/fw876/helloworld.git package/ssr
+# git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git  package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
 # git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
@@ -31,7 +31,7 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lu
 # svn co https://github.com/haiibo/openwrt-packages/trunk/luci-app-argon-config package/luci-app-argon-config
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
-
+git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 ###
 git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
@@ -48,16 +48,16 @@ svn co https://github.com/firker/diy-ziyong/trunk/wrtbwmon package/diy-ziyong/wr
 # svn co https://github.com/firker/diy-ziyong/trunk/theme package/diy-ziyong/theme
 # svn co https://github.com/firker/diy-ziyong/trunk/adguardhome package/diy-ziyong/adguardhome
 # svn co https://github.com/firker/diy-ziyong/trunk/luci-app-adguardhome package/diy-ziyong/luci-app-adguardhome
-find ./ | grep Makefile | grep xray-plugin | xargs rm -f
-svn co https://github.com/sbwml/openwrt_helloworld/trunk/xray-plugin package/xray-plugin
+# find ./ | grep Makefile | grep xray-plugin | xargs rm -f
+# svn co https://github.com/sbwml/openwrt_helloworld/trunk/xray-plugin package/xray-plugin
 ###
 # git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-bypass
 # sed -i 's/luci-lib-ipkg/luci-base/g' package/openwrt-bypass/luci-app-bypass/Makefile
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 # git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
 # git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+# git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
 # git clone https://github.com/dwj0/luci-app-sms-tool.git package/luci-app-sms-tool
 # git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 # git clone https://github.com/linkease/istore.git package/istore
