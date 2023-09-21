e86170aee5379ba5e4841a206f61984e18bde83a
798654 <a798654@gmail.com>
2023-09-21 16:32:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,8 +24,8 @@
 # git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git  package/lua-maxminddb
 # git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
-# svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
-# sed -i 's/luci-lib-ipkg/luci-base/g' package/luci-app-bypass/Makefile
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
+sed -i 's/luci-lib-ipkg/luci-base/g' package/luci-app-bypass/Makefile
 git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 sed -i 's/luci-lib-ipkg/luci-base/g' package/helloworld/luci-app-ssr-plus/Makefile
 # git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
