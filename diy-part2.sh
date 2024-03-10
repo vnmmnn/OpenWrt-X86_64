10d762adff9fa80ca15847827c1a56dcce37dd4a
798654 <a798654@gmail.com>
2024-03-10 23:00:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -38,24 +38,14 @@ rm -rf ./feeds/packages/net/adguardhome
 rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/luci/applications/luci-app-mosdns
 rm -rf ./feeds/packages/net/mosdns
-# rm -rf ./feeds/packages/lang/golang
 rm -rf ./feeds/luci/themes/luci-theme-argon
 # rm -rf ./feeds/luci/themes/luci-theme-argon-mod
 rm -rf ./feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
-# rm -rf ./feeds/packages/net/curl
 ##
 
 rm -rf ./feeds/luci/applications/luci-app-adbyby-plus
 # rm -rf ./package/sirpdboy-package/luci-lib-ipkg
 rm -rf ./package/sirpdboy-package/wrtbwmon
-# rm -rf ./package/sirpdboy-package/luci-app-aria2
-# rm -rf ./package/sirpdboy-package/aria2
-# rm -rf ./package/sirpdboy-package/adguardhome
-# rm -rf ./package/sirpdboy-package/luci-app-adguardhome
-# rm -rf ./package/sirpdboy-package/luci-app-dockerman
-# rm -rf ./package/sirpdboy-package/luci-app-autotimeset
-# rm -rf .package/sirpdboy-package/smartdns
-# rm -rf .package/sirpdboy-package/adguardhome
 ##
 # rm -rf ./package/openwrt-passwall/v2ray-geodata
 rm -rf ./package/helloworld/v2ray-geodata
