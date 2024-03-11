ee3a4d40ed3f80e83ff423368d96e27deed278a9
798654 <a798654@gmail.com>
2024-03-11 16:28:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -40,7 +40,7 @@ rm -rf ./feeds/luci/applications/luci-app-mosdns
 rm -rf ./feeds/packages/net/mosdns
 rm -rf ./feeds/luci/themes/luci-theme-argon
 # rm -rf ./feeds/luci/themes/luci-theme-argon-mod
-rm -rf ./feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+# rm -rf ./feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 ##
 
 rm -rf ./feeds/luci/applications/luci-app-adbyby-plus
