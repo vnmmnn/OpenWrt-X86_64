8cb94498c94e7efd56822b4228cfd35246008fe3
798654 <a798654@gmail.com>
2024-05-19 00:41:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -58,6 +58,8 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 # git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
 ###
 git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
