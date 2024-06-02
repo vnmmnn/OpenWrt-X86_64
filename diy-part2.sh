051d9eb617ef936b8f2ef2ce483e9c07040e5c66
798654 <a798654@gmail.com>
2024-06-03 01:50:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -46,7 +46,7 @@ rm -rf ./feeds/luci/themes/luci-theme-argon
 rm -rf ./feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 rm -rf ./feeds/luci/applications/luci-app-passwall
 rm -rf ./feeds/luci/applications/luci-app-ssr-plus
-rm -rf ./feeds/luci/applications/luci-app-accesscontrol
+
 
 
 ##
