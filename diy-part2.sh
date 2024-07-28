596eaa4f7464d9491593e52fc96923708f64054f
798654 <a798654@gmail.com>
2024-07-28 20:47:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,8 +37,8 @@ rm -rf ./feeds/packages/net/adguardhome
 rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/luci/applications/luci-app-mosdns
 rm -rf ./feeds/packages/net/mosdns
-# rm -rf ./feeds/packages/lang/golang
-# git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+rm -rf ./feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 # rm -rf ./feeds/packages/net/curl
 # git clone https://github.com/sbwml/feeds_packages_net_curl.git feeds/packages/net/curl
 rm -rf ./feeds/luci/themes/luci-theme-argon
