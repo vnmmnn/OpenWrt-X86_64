184f8147e2d8f6a53e98e6c7652be34822250a54
798654 <a798654@gmail.com>
2024-10-06 22:41:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,7 +28,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-kucat/g' ./feeds/luci/collections/luci
 rm -rf ./feeds/luci/applications/luci-app-serverchan
 rm -rf ./feeds/luci/applications/luci-app-upnp
 rm -rf ./feeds/luci/applications/luci-app-smartdns
-rm -rf ./feeds/luci/applications/luci-app-diskman
+# rm -rf ./feeds/luci/applications/luci-app-diskman
 # rm -rf ./feeds/luci/applications/luci-app-aria2
 # rm -rf ./feeds/luci/applications/aria2
 # rm -rf ./feeds/packages/net/miniupnpd
