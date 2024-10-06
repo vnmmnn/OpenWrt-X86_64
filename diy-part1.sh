3df1c7e9b3c6332f0acfbe13fb73422983d45406
798654 <a798654@gmail.com>
2024-10-06 22:47:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -65,13 +65,13 @@ git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 # git clone https://github.com/sirpdboy/luci-theme-opentopd package/luci-theme-opentopd
 # git clone -b js https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat
 git clone https://github.com/sbwml/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/sirpdboy/netspeedtest package/luci-app-netspeedtest
 
 merge_package https://github.com/sirpdboy/sirpdboy-package sirpdboy-package/adguardhome
 merge_package https://github.com/sirpdboy/sirpdboy-package sirpdboy-package/luci-app-adguardhome
-merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/homebox
-merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-diskman
-merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-netspeedtest
-merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-upnp
+# merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/homebox
+# merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-diskman
+# merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-upnp
 merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-theme-kucat
 merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-advancedplus
 merge_package https://github.com/kiddin9/openwrt-packages openwrt-packages/luci-app-partexp
