81d4234581d554ee8c87dce598f2627d4284dc2e
798654 <a798654@gmail.com>
2024-10-13 12:45:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -39,7 +39,7 @@ rm -rf ./feeds/packages/net/alist
 rm -rf ./feeds/luci/applications/luci-app-alist
 rm -rf ./feeds/luci/applications/luci-app-wechatpush
 rm -rf ./feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 # rm -rf ./feeds/packages/net/curl
 # git clone https://github.com/sbwml/feeds_packages_net_curl.git feeds/packages/net/curl
 rm -rf ./feeds/luci/themes/luci-theme-argon
