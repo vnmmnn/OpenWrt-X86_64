65148b5a99fd0d581db3dd31c7a7fb7b87f1abd0
vnmmnn <a798654@gmail.com>
2025-02-23 10:31:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -31,7 +31,7 @@ rm -rf ./feeds/luci/applications/luci-app-smartdns
 # rm -rf ./feeds/luci/applications/luci-app-aria2
 # rm -rf ./feeds/luci/applications/aria2
 # rm -rf ./feeds/packages/net/miniupnpd
-rm -rf ./feeds/packages/net/adguardhome
+# rm -rf ./feeds/packages/net/adguardhome
 rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/luci/applications/luci-app-mosdns
 rm -rf ./feeds/packages/net/mosdns
